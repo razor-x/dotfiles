240a198ff3afffcfc31115a2795620d17030355a
Evan Sosenko <razorx@evansosenko.com>
2025-11-20 22:08:05 -0800

    Only load keys by default if not ssh session

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,5 +1,14 @@
+# Clear Fish greeting.
 set fish_greeting
 
+# Detect SSH session
+set -l is_ssh_session false
+if set -q SSH_CLIENT; or set -q SSH_TTY
+  set is_ssh_session true
+end
+
 if status is-interactive
-  load_private_keys
+  if not is_ssh_session
+    load_private_keys
+  end
 end
