556c2fea5775a7234be2c8847a07a84c841562d0
Evan Sosenko <razorx@evansosenko.com>
2025-11-21 00:47:20 -0800

    Try fish_hybrid_key_bindings

--- home/dot_config/fish/conf.d/bindings.fish
+++ home/dot_config/fish/conf.d/bindings.fish
@@ -1,10 +1,10 @@
+fish_hybrid_key_bindings
+bind -M default vv edit_command_buffer
+
 bind \ce end-of-line accept-autosuggestion
 bind \ca beginning-of-line-or-history-pager
 bind \cf forward-bigword
 bind \cr backward-bigword
-
-fish_vi_key_bindings
-bind -M default vv edit_command_buffer
 
 function beginning-of-line-or-history-pager
   if test -z (commandline)
