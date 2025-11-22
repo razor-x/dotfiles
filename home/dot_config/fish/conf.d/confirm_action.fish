414acc7b88e3a8bdcb43686365bafd93b0c7d003
Evan Sosenko <razorx@evansosenko.com>
2025-11-21 23:16:45 -0800

    Return default_result from confirm_action

--- home/dot_config/fish/conf.d/confirm_action.fish
+++ home/dot_config/fish/conf.d/confirm_action.fish
@@ -1,14 +1,18 @@
 function confirm_action -a message default_action
   if test "$default_action" = "y"
     set prompt "[Y/n]"
+    set default_result 1
   else
     set prompt "[y/N] "
+    set default_result 0
   end
 
   read -P "$message $prompt " -n 1 confirm
 
   switch $confirm
-    case '' Y y
+    case ''
+      return $default_result
+    case Y y
       return 1
     case N n
       return 0
