69325028cadde9890470c068132e6b8681d0f1a8
Evan Sosenko <razorx@evansosenko.com>
2025-11-22 13:19:57 -0800

    Add missing ;

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -6,7 +6,7 @@ if status is-interactive
     load_private_keys
   end
 
-  if set -q SSH_CONNECTION and not set -q TMUX
+  if set -q SSH_CONNECTION; and not set -q TMUX
     set fish_tmux_autostart true
   end
 end
