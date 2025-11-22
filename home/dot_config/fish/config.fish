4f2e03a61ebbc6dc691034cb49c0c3a6151d6274
Evan Sosenko <razorx@evansosenko.com>
2025-11-22 13:17:55 -0800

    Fix syntax

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -2,11 +2,11 @@
 set fish_greeting
 
 if status is-interactive
-  if not set -q SSH_CONNECTION;
+  if not set -q SSH_CONNECTION
     load_private_keys
   end
 
-  if set -q SSH_CONNECTION and not set -q TMUX;
+  if set -q SSH_CONNECTION and not set -q TMUX
     set fish_tmux_autostart true
   end
 end
