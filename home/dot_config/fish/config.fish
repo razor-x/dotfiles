a16896bf7d6d1e80bac30a1f6d402e828662c2ea
Evan Sosenko <razorx@evansosenko.com>
2025-11-21 23:11:31 -0800

    Update tmux prefix

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,18 +1,12 @@
 # Clear Fish greeting.
 set fish_greeting
 
-# Detect SSH session.
-set -l is_ssh_session false
-if set -q SSH_CLIENT; or set -q SSH_TTY
-  set is_ssh_session true
-end
-
 if status is-interactive
-  if not $is_ssh_session
+  if not set -q SSH_CONNECTION;
     load_private_keys
   end
 
-  if $is_ssh_session
+  if set -q SSH_CONNECTION;
     set fish_tmux_autostart true
   end
 end
