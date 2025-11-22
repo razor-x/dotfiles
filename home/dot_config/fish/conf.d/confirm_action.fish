d300c06ca9c4a24f925ccb21061342eeb969ef9e
Evan Sosenko <razorx@evansosenko.com>
2025-11-21 23:21:25 -0800

    Fix logic in confirm_action

--- home/dot_config/fish/conf.d/confirm_action.fish
+++ home/dot_config/fish/conf.d/confirm_action.fish
@@ -1,20 +1,21 @@
 function confirm_action -a message default_action
   if test "$default_action" = 'y'
     set prompt '[Y/n]'
-    set default_result 1
-  else
-    set prompt '[y/N] '
     set default_result 0
+  else
+    set prompt '[y/N]'
+    set default_result 1
   end
 
   read -P "$message $prompt " -n 1 confirm
+  echo
 
   switch $confirm
     case ''
       return $default_result
     case Y y
-      return 1
-    case N n
       return 0
+    case N n
+      return 1
   end
 end
