26edd6d34fbba8c145233647ed0ca8a93df223ce
Evan Sosenko <razorx@evansosenko.com>
2025-11-22 16:03:22 -0800

    Add fish comments

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -2,15 +2,17 @@
 set fish_greeting
 
 if status is-interactive
-  # Use simple fish prompt in Linux  virtual console.
+  # Use simple fish prompt in Linux virtual console.
   if test "$TERM" = "linux"
     fish_config prompt choose default
   end
 
+  # Limit when prompted to load keys.
   if not set -q SSH_CONNECTION; and test "$TERM" != "linux"
     load_private_keys
   end
 
+  # Autostart tmux when connecting though SSH and client is not using tmux.
   if set -q SSH_CONNECTION; and not set -q TMUX
     set fish_tmux_autostart true
   end
