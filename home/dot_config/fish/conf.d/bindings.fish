d93273564a3466c2f508d96c20609f745286d612
Evan Sosenko <razorx@evansosenko.com>
2025-11-23 22:17:33 -0800

    Bind keys to insert mode

--- home/dot_config/fish/conf.d/bindings.fish
+++ home/dot_config/fish/conf.d/bindings.fish
@@ -1,10 +1,10 @@
 fish_hybrid_key_bindings
 bind --mode default vv edit_command_buffer
 
-bind \ce --mode default end-of-line accept-autosuggestion
-bind \ca --mode default beginning-of-line-or-history-pager
-bind \cf --mode default forward-word
-bind \cr --mode default backward-word
+bind ctrl-e --mode insert end-of-line accept-autosuggestion
+bind ctrl-a --mode insert beginning-of-line-or-history-pager
+bind ctrl-f --mode insert forward-word
+bind ctrl-r --mode insert backward-word
 
 function beginning-of-line-or-history-pager
   if test -z (commandline)
