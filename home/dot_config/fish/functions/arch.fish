9093c8a0ad093d9f43c6b2afba56c80b2fcdd61b
Evan Sosenko <razorx@evansosenko.com>
2025-11-23 22:06:30 -0800

    Add arch_package_optional_deps

---
+++ home/dot_config/fish/functions/arch.fish
@@ -0,0 +1,13 @@
+function arch_package_optional_deps () {
+    pacman -Qi | awk '
+      /^Name/ {pkg = $3}
+      /^Optional Deps/ {in_opt = 1; found = 0; next}
+      in_opt {
+        if (/^[A-Z]/) {in_opt = 0}
+        else if (!/None/ && !/installed/) {
+          if (!found) {print "\n" pkg ":"; found = 1}
+          print "  " $0
+        }
+      }
+    '
+}
