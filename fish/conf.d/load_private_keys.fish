d505f581832350d0c346722c52333964e5ad9aef
Evan Sosenko <razorx@evansosenko.com>
2025-11-26 22:40:31 -0800

    Split fish key loading functions

--- fish/conf.d/load_private_keys.fish
+++ fish/conf.d/load_private_keys.fish
@@ -1,12 +1,4 @@
-function load_private_keys --on-variable fish_autoload_private_keys
-  if test "$fish_autoload_private_keys" != true
-    return
-  end
-
-  if not confirm_action 'Load SSH and GPG keys?' 'y'
-    return
-  end
-
+function load_private_keys
   # Load all SSH keys into the ssh-agent.
   if not ssh-add -l &>/dev/null
     ssh-add
@@ -16,4 +8,16 @@ function load_private_keys --on-variable fish_autoload_private_keys
   if not gpg-connect-agent 'keyinfo --list' /bye 2>/dev/null | grep -q '1 P'
     echo '' | gpg --clearsign --armor &>/dev/null
   end
+end
+
+function autoload_private_keys --on-variable fish_autoload_private_keys
+  if test "$fish_autoload_private_keys" != true
+    return
+  end
+
+  if not confirm_action 'Load SSH and GPG keys?' 'y'
+    return
+  end
+
+  load_private_keys
 end
