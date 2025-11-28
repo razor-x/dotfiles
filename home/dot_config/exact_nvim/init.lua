52f3c9ffea0c4689b4dbe12d44c3f116eefb6a60
Evan Sosenko <razorx@evansosenko.com>
2025-11-28 15:59:07 -0800

    Enable mini.pick

--- home/dot_config/exact_nvim/init.lua
+++ home/dot_config/exact_nvim/init.lua
@@ -1,1 +1,13 @@
 require("config").setup()
+local pick = require("mini.pick")
+pick.setup()
+
+vim.keymap.set('n', '<leader>e', function()
+  pick.builtin.files({
+    tool = 'git'  -- Uses git ls-files (tracked files only)
+  })
+end, { desc = 'Pick git tracked files' })
+
+vim.keymap.set('n', '<leader>f', function()
+  pick.builtin.files()  -- Defaults to 'rg' which respects .gitignore and global ignore
+end, { desc = 'Pick all files in cwd' })
