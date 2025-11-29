9cdeca162923d6e77a700e8bd99550e796ef4e34
Evan Sosenko <razorx@evansosenko.com>
2025-11-29 00:42:09 -0800

    Add close message

--- home/dot_config/fish/functions/zellij_scrollback.fish
+++ home/dot_config/fish/functions/zellij_scrollback.fish
@@ -1,8 +1,15 @@
 function zellij_scrollback \
     --description 'Save the Zellij scrollback and view it.'
 
+    set zellij_dump "/tmp/zellij_scrollback.dump.txt"
+
+    if not test -f $zellij_dump
+        echo "Already saved and viewed the last dump, press Ctrl-C to close..."
+        return 1
+    end
+
     set zellij_scrollback "$HOME/scrollback.$(now).txt"
-    mv /tmp/zellij_scrollback.dump.txt $zellij_scrollback
+    mv $zellij_dump $zellij_scrollback
     bat -p $zellij_scrollback
     echo $zellij_scrollback
 end
