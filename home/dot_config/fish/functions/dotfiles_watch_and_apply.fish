332f267e7d49d7d4d726520fa86b0990dbc73c9f
Evan Sosenko <razorx@evansosenko.com>
2025-11-30 21:04:53 -0800

    Update dotfiles_watch_and_apply to check new files

--- home/dot_config/fish/functions/dotfiles_watch_and_apply.fish
+++ home/dot_config/fish/functions/dotfiles_watch_and_apply.fish
@@ -1,5 +1,7 @@
 function dotfiles_watch_and_apply \
     --description "Watch and apply changes to dotfiles"
 
-    fd . (chezmoi source-path) -t file | entr chezmoi apply
+    while sleep 0.1
+        fd . (chezmoi source-path) -t file | entr -d chezmoi apply
+    end
 end
