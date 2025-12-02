fadfd53fcd0b1264860e2720676831bfb9d437d9
Evan Sosenko <razorx@evansosenko.com>
2025-12-01 23:10:47 -0800

    Sleep for 2 seconds

--- home/dot_config/fish/functions/dotfiles_watch_and_apply.fish
+++ home/dot_config/fish/functions/dotfiles_watch_and_apply.fish
@@ -3,9 +3,12 @@ function dotfiles_watch_and_apply \
 
     set src (chezmoi source-path)
     echo "Watching $src for changes..."
-    while sleep 0.1
+
+    # All attempts to properly handle SIGINT via entr have failed.
+    # Provide a 2 second window after entr exits for a second Ctrl-C
+    # to exit the function.
+    while sleep 2
         fd . $src -t file | entr -d chezmoi apply
-        or break
         echo "File added to $src..."
     end
 end
