efd52015532328ddb7ac98810288f9ae1fa75805
Evan Sosenko <razorx@evansosenko.com>
2025-12-01 22:50:41 -0800

    Exit loop in dotfiles_watch_and_apply on ctrl-c

--- home/dot_config/fish/functions/dotfiles_watch_and_apply.fish
+++ home/dot_config/fish/functions/dotfiles_watch_and_apply.fish
@@ -1,7 +1,11 @@
 function dotfiles_watch_and_apply \
     --description "Watch and apply changes to dotfiles"
 
+    set src (chezmoi source-path)
+    echo "Watching $src for changes..."
     while sleep 0.1
-        fd . (chezmoi source-path) -t file | entr -d chezmoi apply
+        fd . $src -t file | entr -d chezmoi apply
+        or break
+        echo "File added to $src..."
     end
 end
