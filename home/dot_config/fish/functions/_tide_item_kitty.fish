e4b3d8827d06569cd78f7cb88b1c6146b3f0afe8
Evan Sosenko <razorx@evansosenko.com>
2025-12-02 23:28:48 -0800

    Update _tide_item_kitty

--- home/dot_config/fish/functions/_tide_item_kitty.fish
+++ home/dot_config/fish/functions/_tide_item_kitty.fish
@@ -4,5 +4,17 @@ set --universal tide_kitty_bg_color 'normal'
 function _tide_item_kitty \
     --description "Tide prompt item that shows the active kitty mode"
 
-   _tide_print_item   kitty          $tide_kitty_icon' ' n
+    if not set -q KITTY_WINDOW_ID
+        return
+    end
+
+    set mode (kitten @ ls --match id:$KITTY_WINDOW_ID \
+        | jq -r '.[0].tabs[0].windows[0].user_vars.mode // "insert"')
+
+    switch $mode
+        case 'insert'
+            _tide_print_item 'kitty' $tide_kitty_icon
+        case 'normal'
+            _tide_print_item 'kitty' $tide_kitty_icon 'n'
+    end
 end
