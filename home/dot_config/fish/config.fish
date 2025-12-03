7c37d1693ac1c16725bdf03654912469ba9e2668
Evan Sosenko <razorx@evansosenko.com>
2025-12-02 21:55:44 -0800

    Do not start zellij if in kitty

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,11 +1,11 @@
 # Clear Fish greeting.
-set -g fish_greeting
+set --global fish_greeting
 
 if status is-interactive
     # Configure pinentry to use the correct TTY.
     # This must be done separately for each shell.
     # https://wiki.archlinux.org/title/GnuPG#Configure_pinentry_to_use_the_correct_TTY
-    set -g GPG_TTY (tty)
+    set --global GPG_TTY (tty)
     gpg-connect-agent updatestartuptty /bye >/dev/null
 
     # Use simple fish prompt in Linux virtual console.
@@ -14,12 +14,16 @@ if status is-interactive
     end
 
     # Limit when prompted to load keys.
-    if not set -q SSH_CONNECTION; and test "$TERM" != linux
+    if not set --query SSH_CONNECTION; and test "$TERM" != linux
         set fish_autoload_private_keys true
     end
 
     # Autostart Zellij when connecting though SSH and not multiplexing.
-    if set -q SSH_CONNECTION; and not set -q TMUX; and not set -q ZELLIJ
+    if set --query SSH_CONNECTION; \
+        and not set --query TMUX;
+        and not set -query ZELLIJ;
+        and not set -query KITTY_PID
+
         zellij attach --create ssh
         zellij delete-session ssh
         kill $fish_pid
