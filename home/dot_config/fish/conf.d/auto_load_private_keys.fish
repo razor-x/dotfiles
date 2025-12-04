4ecf7f8ea2edae73569d809dd41d801557d8f79e
Evan Sosenko <razorx@evansosenko.com>
2025-12-03 20:46:42 -0800

    Move more fish config behind is-interactive

--- home/dot_config/fish/conf.d/auto_load_private_keys.fish
+++ home/dot_config/fish/conf.d/auto_load_private_keys.fish
@@ -1,10 +1,10 @@
 function load_private_keys \
     --description 'Load SSH keys into the ssh-agent and GPG keys into the GPG agent'
+
     if not ssh-add -l &>/dev/null
         ssh-add
     end
 
-    # Load GPG key into the ssh-agent.
     if not gpg-connect-agent 'keyinfo --list' /bye 2>/dev/null | grep -q '1 P'
         echo '' | gpg --clearsign --armor &>/dev/null
     end
--- home/dot_config/fish/conf.d/tui.fish
+++ home/dot_config/fish/conf.d/tui.fish
@@ -1,59 +1,59 @@
-# Ag
-abbr a rg
-abbr aa batgrep
-
-# dir
-# abbr d d
-
-# edit
-abbr e $VISUAL
-
-# list
-alias l 'eza -l'
-alias ll 'eza -1'
-alias la 'eza -la'
-alias ls 'eza'
-alias lsp list_paginate
-
-# eXtract
-abbr x ouch d
-
-# view
-abbr v bat -p
-abbr vv prettybat -p
-
-# MultipleX
-abbr mx zellij
-
-# monitor
-abbr mon zenith
-
-abbr dl xh -d
-abbr dif delta
-
-abbr gg gitui
-alias gs git_idempotent_switch
-
-# Colorize help text using bat.
-abbr -a --position anywhere -- --help '--help | bat -plhelp'
-
-abbr rmf rm -rf
-
-alias d yazi_change_cwd
-
-abbr cljrepl lein repl
-abbr pyrepl bpython
-abbr rbrepl pry
-
-abbr jsrepl node
-abbr fmtjs prettier -w --single-quote --jsx-single-quote --no-semi
-
-abbr archrc cd "$ACONFMGR_CONFIG/.."
-
-abbr dotfiles cd "(chezmoi source-path)"
-alias dotwatch dotfiles_watch_and_apply
-
 if status is-interactive
+    # Ag
+    abbr a rg
+    abbr aa batgrep
+
+    # dir
+    # abbr d d
+
+    # edit
+    abbr e $VISUAL
+
+    # list
+    alias l 'eza -l'
+    alias ll 'eza -1'
+    alias la 'eza -la'
+    alias ls 'eza'
+    alias lsp list_paginate
+
+    # eXtract
+    abbr x ouch d
+
+    # view
+    abbr v bat -p
+    abbr vv prettybat -p
+
+    # MultipleX
+    abbr mx zellij
+
+    # monitor
+    abbr mon zenith
+
+    abbr dl xh -d
+    abbr dif delta
+
+    abbr gg gitui
+    alias gs git_idempotent_switch
+
+    # Colorize help text using bat.
+    abbr -a --position anywhere -- --help '--help | bat -plhelp'
+
+    abbr rmf rm -rf
+
+    alias d yazi_change_cwd
+
+    abbr cljrepl lein repl
+    abbr pyrepl bpython
+    abbr rbrepl pry
+
+    abbr jsrepl node
+    abbr fmtjs prettier -w --single-quote --jsx-single-quote --no-semi
+
+    abbr archrc cd "$ACONFMGR_CONFIG/.."
+
+    abbr dotfiles cd "(chezmoi source-path)"
+    alias dotwatch dotfiles_watch_and_apply
+
     # Use batpipe as the less preprocessor.
     eval (batpipe)
 
@@ -62,10 +62,9 @@ if status is-interactive
 
     # Use zoxide.
     zoxide init --cmd j fish | source
+
+    alias now='date +"%Y-%m-%dT%H:%M:%S.%3N%:z"'
+    alias nowutc='date --utc +"%Y-%m-%dT%H:%M:%S.%3N%:z"'
+    alias nowz='date --utc +"%Y-%m-%dT%H:%M:%S.%3NZ"'
+    alias today='date --iso-8601=date'
 end
-
-
-alias now='date +"%Y-%m-%dT%H:%M:%S.%3N%:z"'
-alias nowutc='date --utc +"%Y-%m-%dT%H:%M:%S.%3N%:z"'
-alias nowz='date --utc +"%Y-%m-%dT%H:%M:%S.%3NZ"'
-alias today='date --iso-8601=date'
