71dbb279607a75d074a92328624fd74777ebae5d
Evan Sosenko <razorx@evansosenko.com>
2025-12-10 00:17:47 -0800

    Update all fish binds

--- home/dot_config/fish/functions/fish_user_key_bindings.fish
+++ home/dot_config/fish/functions/fish_user_key_bindings.fish
@@ -3,50 +3,72 @@ function fish_user_key_bindings \
 
     fish_hybrid_key_bindings
 
+    # Erase all Ctrl and Alt insert mode bindings.
+    bind \
+        | grep --extended-regexp --no-ignore-case 'bind --preset -M insert (ctrl|alt)' \
+        # Keep ctrl-space which inserts a literal space without expanding abbreviations.
+        | grep --invert-match 'ctrl-space' \
+        # Keep escape (ctrl-[) which toggles normal and insert mode.
+        | grep --invert-match 'ctrl-\[' \
+        | string replace -r '^bind\s+(.*?)((ctrl|alt)-\S+)\s.*' 'bind --erase $1$2' \
+        | source
+
     bind --mode insert 'ctrl-tab' _atuin_search
 
     fzf_configure_bindings \
-        --directory='ctrl-;' \
+        --directory='ctrl-i' \
         --git_log="ctrl-'" \
-        --git_status='ctrl-g' \
+        --git_status='ctrl-;' \
         --history='' \
         --processes='ctrl-\\' \
-        --variables='ctrl-.'
-
-    # Erase all Alt bindings.
-    bind \
-        | grep alt- \
-        | string replace -r '^bind\s+(.*?)(alt-\S+)\s.*' 'bind --erase $1$2' \
-        | source
-
-    # Erase conflicting navigation bindings.
-    bind \
-        | grep 'ctrl-[hjkl] ' \
-        | string replace -r '^bind\s+(.*?)(ctrl-\S+)\s.*' 'bind --erase $1$2' \
-        | source
+        --variables='ctrl-x'
 
     bind --mode default vv edit_command_buffer
+    bind --mode insert ctrl-m edit_command_buffer
 
-    bind ctrl-p --mode insert up-or-search
-    bind ctrl-n --mode insert down-or-search
-    bind ctrl-e --mode insert end-of-line accept-autosuggestion
-    bind ctrl-a --mode insert beginning-of-line-or-history-pager
-    bind ctrl-f --mode insert forward-word
-    bind ctrl-r --mode insert backward-word
-    bind ctrl-b --mode insert backward-delete-char
-    bind ctrl-y --mode insert backward-delete-char
-    bind ctrl-o --mode insert forward-char
-    bind ctrl-u --mode insert backward-char
-    bind ctrl-q --mode insert fish_paginate
-    # TODO: Consider ctrl-shift-r backward-bigword, etc.
-    # TODO: Consider ctrl-shift-u backward-path-component, etc.
+    # Navigate history.
+    bind --mode insert ctrl-p up-or-search
+    bind --mode insert ctrl-n down-or-search
 
-    bind ctrl-/ --mode insert __fish_whatis_current_token
-    bind ctrl-/ --mode visual __fish_whatis_current_token
+    # Navigate prompt.
+    bind --mode insert ctrl-o forward-char
+    bind --mode insert ctrl-u backward-char
+    bind --mode insert ctrl-f forward-word
+    bind --mode insert ctrl-r backward-word
+    bind --mode insert ctrl-shift-f forward-token
+    bind --mode insert ctrl-shift-r backward-token
+    bind --mode insert ctrl-a beginning-of-line-or-history-pager
+    bind --mode insert ctrl-e end-of-line accept-autosuggestion
+
+    # Delete from prompt.
+    bind --mode insert ctrl-b backward-delete-char
+    bind --mode insert ctrl-y backward-delete-char
+    bind --mode insert ctrl-w backward-kill-word
+    bind --mode insert ctrl-shift-w backward-kill-token
+    bind --mode insert ctrl-backspace backward-kill-path-component
+    bind --mode insert ctrl-delete kill-line
+
+    # Manipulate prompt.
+    bind --mode insert ctrl-t transpose-chars
+    bind --mode insert ctrl-q fish_paginate
+    # bind --mode insert ctrl-g fish_filter
+    bind --mode insert ctrl-z undo
+    bind --mode insert ctrl-shift-z redo
+
+    # Access clipboard.
+    bind ctrl-enter fish_clipboard_paste
+    bind --mode insert ctrl-c clear-commandline repaint-mode
+    bind --mode insert ctrl-v fish_clipboard_paste
+    ## TODO bind , (<) to insert a (fish_clipboard_paste) and . (>) to pipe to fish_clipboard_copy
+
+    # Misc.
+    bind --mode insert ctrl-/ __fish_whatis_current_token
+    bind --mode visual ctrl-/ __fish_whatis_current_token
+    bind --mode insert ctrl-d delete-or-exit
 end
 
 function beginning-of-line-or-history-pager \
-    --description 'Move the cursor to the beginning of the line; if already there, open the history pager'
+    --description 'Move the cursor to the beginning of the line; if empty, open the history pager'
 
     if test -z (commandline)
         commandline -f history-pager
