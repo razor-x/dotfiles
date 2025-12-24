71318ee717c7f2dd667e40638319a83945823af5
Evan Sosenko <razorx@evansosenko.com>
2025-12-23 16:54:50 -0800

    Add smart rg wrapper

---
+++ home/dot_config/fish/functions/rg.fish
@@ -0,0 +1,10 @@
+function rg \
+    --wraps 'rg' \
+    --description "Wrap ripgrep to search hidden files when inside a git repository"
+
+    if git rev-parse --is-inside-work-tree &>/dev/null
+        command rg --hidden $argv
+    else
+        command rg $argv
+    end
+end
