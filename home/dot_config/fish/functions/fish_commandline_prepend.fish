b57a731e1cd90c2115d44c269a10dc7e5d4d35c6
Evan Sosenko <razorx@evansosenko.com>
2025-12-26 23:48:39 -0800

    Patch fish_commandline_prepend
    https://github.com/fish-shell/fish-shell/issues/8763#issuecomment-1059977742

---
+++ home/dot_config/fish/functions/fish_commandline_prepend.fish
@@ -0,0 +1,28 @@
+# UPSTREAM: The fish_commandline_prepend function will not toggle if any meta character is present.
+# https://github.com/fish-shell/fish-shell/issues/8763
+function fish_commandline_prepend --description "Prepend the given string to the command-line, or remove the prefix if already there"
+    if not commandline | string length -q
+        commandline -r $history[1]
+    end
+
+    set -l process (commandline | string collect)
+
+    set -l to_prepend "$argv[1] "
+    if string match -qr '^ ' "$process"
+        set to_prepend " $argv[1]"
+    end
+
+    set -l length_diff (string length -- "$to_prepend")
+    set -l cursor_location (commandline -C)
+
+    set -l escaped (string escape --style=regex -- $to_prepend)
+    if set process (string replace -r -- "^$escaped" "" $process)
+        commandline --replace -- $process
+        set length_diff "-$length_diff"
+    else
+        commandline -C 0
+        commandline -i -- "$to_prepend"
+    end
+
+    commandline -C (math "max 0,($cursor_location + $length_diff)")
+end
