3bdda3d5aa339157bd678eb84e8acbe13b7c20f2
Evan Sosenko <razorx@evansosenko.com>
2025-12-27 00:03:57 -0800

    Fix beginning-of-line check

--- home/dot_config/fish/functions/backward-word-or-atuin.fish
+++ home/dot_config/fish/functions/backward-word-or-atuin.fish
@@ -1,9 +1,9 @@
 function backward-word-or-atuin \
     --description 'Move the cursor back one work; if empty, open Atuin'
 
-    if test -z (commandline)
-        _atuin_search
-    else
+    if string length --quiet -- (commandline)
         commandline --function backward-word
+    else
+        _atuin_search
     end
 end
--- home/dot_config/fish/functions/beginning-of-line-or-history-pager.fish
+++ home/dot_config/fish/functions/beginning-of-line-or-history-pager.fish
@@ -1,9 +1,9 @@
 function beginning-of-line-or-history-pager \
     --description 'Move the cursor to the beginning of the line; if empty, open the history pager'
 
-    if test -z (commandline)
-        commandline --function history-pager
-    else
+    if string length --quiet -- (commandline)
         commandline --function beginning-of-line
+    else
+        commandline --function history-pager
     end
 end
