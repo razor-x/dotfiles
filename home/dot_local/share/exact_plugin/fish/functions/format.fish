a88a3cfa474595607ea55b12977fe3728e3fe36a
Evan Sosenko <razorx@evansosenko.com>
2026-01-04 11:33:59 -0800

    Add config-precedence to prettier

--- home/dot_local/share/exact_plugin/fish/functions/format.fish
+++ home/dot_local/share/exact_plugin/fish/functions/format.fish
@@ -74,7 +74,9 @@ function format \
                 set --append cmd $file
             end
         case .js .jsx .ts .tsx
-            set --function cmd prettier --single-quote --jsx-single-quote --no-semi
+            set --function cmd prettier \
+                --single-quote --jsx-single-quote --no-semi \
+                --config-precedence file-override
             if $write_to_file
                 set --append cmd --write
             end
