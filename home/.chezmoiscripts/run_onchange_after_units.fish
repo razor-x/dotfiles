a0358eb7f95ba58e021d4fd227e70a99b607e30a
Evan Sosenko <razorx@evansosenko.com>
2026-01-03 21:17:19 -0800

    Add units_to_disable to run_onchange_after_units

--- home/.chezmoiscripts/run_onchange_after_units.fish
+++ home/.chezmoiscripts/run_onchange_after_units.fish
@@ -2,16 +2,24 @@
 
 # hash: {{ template "user_env_hash" . }}
 
-set --local units \
+# Do not delete items from this list: move them to units_to_disable instead.
+set --local units_to_enable \
     atuin \
     ssh-agent
+
+set --local units_to_disable
 
 if type --query systemctl
     systemctl --user daemon-reload
 
-    for unit in $units
+    for unit in $units_to_enable
         systemctl --user enable $unit
         systemctl --user restart $unit
+    end
+
+    for unit in $units_to_disable
+        systemctl --user stop $unit
+        systemctl --user disable $unit
     end
 else
     echo 'Cannot setup systemd user units: systemctl not found.'
