52cff07d5e982b92c08d8fd5469bc7b651c4a3a5
Evan Sosenko <razorx@evansosenko.com>
2026-01-03 21:49:39 -0800

    Allow format to handle stdin and stdout

--- home/dot_local/share/exact_plugin/fish/functions/format.fish
+++ home/dot_local/share/exact_plugin/fish/functions/format.fish
@@ -1,25 +1,52 @@
 function format \
-    --argument-names file \
     --description 'Format a file with an appropriate code formatter; writes in-place unless piped'
 
-    set --function type (path extension $file)
+    argparse 'e/extension=' -- $argv
+    or return
 
-    # Check if stdout is a TTY (not being piped)
     if isatty stdout
-        set --function write_flag --write
+        set --function use_stdout true
     else
-        set --function write_flag
+        set --function use_stdout false
+    end
+
+    if set --query _flag_extension
+        set --function type $_flag_extension
+
+        # Normalize the extension to start with a dot.
+        if not string match --quiet '.*' $type
+            set type .$type
+        end
+
+        if not isatty stdin
+            set --function use_stdin true
+        else
+            echo "Error: using -e/--extension requires input from stdin"
+            return 1
+        end
+    else if test (count $argv) -eq 1
+        set --function type (path extension $argv[1])
+        set --function file $argv[1]
+        set --function use_stdin false
+    else
+        echo "Usage: format FILE or cat FILE | format --extension EXT"
+        return 1
     end
 
     switch $type
         case .js .jsx .ts .tsx
-            prettier \
-                --single-quote \
-                --jsx-single-quote \
-                --no-semi \
-                $write_flag \
-                $file
+            set --local cmd prettier --single-quote --jsx-single-quote --no-semi
+            if $use_stdin
+                set --append cmd --parser typescript
+            else
+                set --append cmd $file
+            end
+            if $use_stdout
+                set --append cmd --write
+            end
+            $cmd
         case '*'
             echo "No formatter available for $type file extension"
+            return 2
     end
 end
