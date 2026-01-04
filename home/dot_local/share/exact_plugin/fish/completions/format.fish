2a2c4a1431742b46453a1e07e1ee4c82e5328dc8
Evan Sosenko <razorx@evansosenko.com>
2026-01-03 21:53:46 -0800

    Add completions for format

---
+++ home/dot_local/share/exact_plugin/fish/completions/format.fish
@@ -0,0 +1,7 @@
+complete \
+    --command format \
+    --short-option e \
+    --long-option extension \
+    --description 'Specify file extension when reading from stdin' \
+    --exclusive \
+    --arguments 'js jsx ts tsx'
