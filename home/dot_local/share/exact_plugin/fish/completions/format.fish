3457000f9269f9e84db1376d4d24dd19a13bfd0a
Evan Sosenko <razorx@evansosenko.com>
2026-01-05 21:15:47 -0800

    Improve completions

--- home/dot_local/share/exact_plugin/fish/completions/format.fish
+++ home/dot_local/share/exact_plugin/fish/completions/format.fish
@@ -1,7 +1,26 @@
+function __fish_complete_format_files
+    set --local token (commandline --cut-at-cursor --current-token)
+    for file in $token*.{fish,go,js,jsx,ts,tsx,bash,sh,zsh} $token*/
+        if test -e $file
+            echo $file
+        end
+    end
+end
+
 complete \
     --command format \
     --short-option e \
     --long-option extension \
-    --description 'Language' \
     --exclusive \
-    --arguments 'fish go js jsx ts tsx bash sh zsh'
+    --arguments 'fish go js jsx ts tsx bash sh zsh' \
+    --description 'Syntax'
+
+complete \
+    --command format \
+    --no-files \
+    --condition 'not __fish_seen_argument --short e --long extension' \
+    --arguments '(__fish_complete_format_files)'
+
+complete \
+    --command format \
+    --condition '__fish_seen_argument --short e --long extension'
