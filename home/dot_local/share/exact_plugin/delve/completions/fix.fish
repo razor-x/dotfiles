ca0076b38573bcff92e7d4fe7538c181e8c4295d
Evan Sosenko <razorx@evansosenko.com>
2026-01-10 11:34:23 -0800

    Add delve fix function

---
+++ home/dot_local/share/exact_plugin/delve/completions/fix.fish
@@ -0,0 +1,45 @@
+function __fish_complete_fix_files
+    set --function token \
+        (string replace --regex '\.$' '' -- (commandline --cut-at-cursor --current-token))
+
+    for file in \
+        $token*.c \
+        $token*.go \
+        $token*.js \
+        $token*.jsx \
+        $token*.ts \
+        $token*.tsx \
+        $token*/
+        if test -e $file
+            echo $file
+        end
+    end
+end
+
+function __fish_complete_fix_extensions
+    printf '%s\t%s\n' \
+        c C \
+        go Go \
+        js JavaScript \
+        jsx 'JavaScript JSX' \
+        ts TypeScript \
+        tsx 'TypeScript JSX'
+end
+
+complete \
+    --command fix \
+    --short-option e \
+    --long-option extension \
+    --exclusive \
+    --arguments '(__fish_complete_fix_extensions)' \
+    --description 'Explicitly set file extension'
+
+complete \
+    --command fix \
+    --no-files \
+    --condition 'not __fish_seen_argument --short e --long extension' \
+    --arguments '(__fish_complete_fix_files)'
+
+complete \
+    --command fix \
+    --condition '__fish_seen_argument --short e --long extension'
---
+++ home/dot_local/share/exact_plugin/delve/functions/fix.fish
@@ -0,0 +1,50 @@
+function fix \
+    --description 'Fix linter issues in a file with an appropriate code linter'
+
+    argparse 'e/extension=' -- $argv
+    or return
+
+    if set --query --function _flag_extension
+        if not string match --quiet '.*' $_flag_extension
+            set --function extension .$_flag_extension
+        else
+            set --function extension $_flag_extension
+        end
+    end
+
+    if test (count $argv) -ne 1
+        echo 'usage: fix [(-e | --extension) EXT] FILE'
+        return 1
+    end
+
+    set --function file $argv[1]
+
+    if not test -f "$file"
+        echo "fix: no file exists named $file"
+        return 1
+    end
+
+    if not set --query --function extension
+        set --function extension (path extension $file)
+    end
+
+    if test -z "$extension"
+        echo "fix: cannot fix files missing a file extension: $file"
+        echo "use 'fix -e EXT FILE' to fix this file"
+        return 1
+    end
+
+    switch $extension
+        case .c
+            set --function cmd clang-tidy --fix-errors $file
+        case .go
+            set --function cmd golangci-lint run --fix $file
+        case .js .jsx .ts .tsx
+            set --function cmd biome lint --write $file
+        case '*'
+            echo "fix: no fixer available for $extension files"
+            return 2
+    end
+
+    $cmd
+end
