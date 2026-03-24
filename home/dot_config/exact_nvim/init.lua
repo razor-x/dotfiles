e4f3ae93f27a9ae3b4385e12fa600bff20eb1364
Evan Sosenko <razorx@evansosenko.com>
2026-03-23 23:35:50 -0700

    Make <C-l> pmenu_accept

--- home/dot_config/exact_nvim/init.lua
+++ home/dot_config/exact_nvim/init.lua
@@ -63,7 +63,7 @@ vim.lsp.enable("fish_lsp")
 local map_multistep = require("mini.keymap").map_multistep
 map_multistep("i", "<Tab>", { "pmenu_next" })
 map_multistep("i", "<S-Tab>", { "pmenu_prev" })
-map_multistep("i", "<CR>", { "pmenu_accept", "minipairs_cr" })
+map_multistep("i", "<C-l>", { "pmenu_accept", "minipairs_cr" })
 map_multistep("i", "<BS>", { "minipairs_bs" })
 
 vim.keymap.set("i", "<C-e>", function()
