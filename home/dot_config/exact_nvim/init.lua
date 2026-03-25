2982f1f7664be7025b05afed6ae862bdbb3e5e56
Evan Sosenko <razorx@evansosenko.com>
2026-03-25 00:11:49 -0700

    Prefer single lua quotes

--- home/dot_config/exact_nvim/init.lua
+++ home/dot_config/exact_nvim/init.lua
@@ -46,24 +46,24 @@ vim.lsp.enable("ts_ls")
 vim.lsp.enable("biome")
 
 --- https://github.com/neovim/nvim-lspconfig/blob/841c6d4139aedb8a3f2baf30cef5327371385b93/lsp/lua_ls.lua#L16-L61
-vim.lsp.config('lua_ls', {
+vim.lsp.config("lua_ls", {
   on_init = function(client)
     if client.workspace_folders then
       local path = client.workspace_folders[1].name
       if
-        path ~= vim.fn.stdpath('config')
-        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+        path ~= vim.fn.stdpath("config")
+        and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
       then
         return
       end
     end
 
-    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
       runtime = {
-        version = 'LuaJIT',
+        version = "LuaJIT",
         path = {
-          'lua/?.lua',
-          'lua/?/init.lua',
+          "lua/?.lua",
+          "lua/?/init.lua",
         },
       },
       workspace = {
