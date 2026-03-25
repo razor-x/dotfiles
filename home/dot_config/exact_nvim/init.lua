a0340c1e8c13aaaa5a834b9197cbdd1cbccd3c49
Evan Sosenko <razorx@evansosenko.com>
2026-03-24 23:55:28 -0700

    Use neovim config for lua_ls

--- home/dot_config/exact_nvim/init.lua
+++ home/dot_config/exact_nvim/init.lua
@@ -45,7 +45,40 @@ vim.lsp.enable("golangci_lint_ls")
 vim.lsp.enable("ts_ls")
 vim.lsp.enable("biome")
 
+--- https://github.com/neovim/nvim-lspconfig/blob/841c6d4139aedb8a3f2baf30cef5327371385b93/lsp/lua_ls.lua#L16-L61
+vim.lsp.config('lua_ls', {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath('config')
+        and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        version = 'LuaJIT',
+        path = {
+          'lua/?.lua',
+          'lua/?/init.lua',
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
+  },
+})
 vim.lsp.enable("lua_ls")
+
 vim.lsp.enable("stylua")
 
 vim.lsp.enable("phpactor")
