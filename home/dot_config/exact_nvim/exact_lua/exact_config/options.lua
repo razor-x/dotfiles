d96f6151d113d0a12b866b8f6de98514105eb4b0
Evan Sosenko <razorx@evansosenko.com>
2026-04-07 23:58:51 -0700

    Add softtabstop

--- home/dot_config/exact_nvim/exact_lua/exact_config/options.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_config/options.lua
@@ -25,6 +25,7 @@ function M.setup()
   -- Set indentation preferences.
   vim.opt.smartindent = true
   vim.opt.tabstop = 2
+  vim.opt.softtabstop = 2
   vim.opt.shiftwidth = 2
   vim.opt.expandtab = true
   vim.opt.autoindent = true
