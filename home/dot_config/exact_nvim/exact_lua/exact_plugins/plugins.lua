8e3438a477f4555704a03e552b548f8a1046f30f
Evan Sosenko <razorx@evansosenko.com>
2026-04-13 00:13:53 -0700

    Move mini config to plugins.lua

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/plugins.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/plugins.lua
@@ -1,6 +1,34 @@
 ---@type LazySpec
 return {
-  { "nvim-mini/mini.nvim", version = "*" },
+  {
+    "nvim-mini/mini.nvim",
+    config = function()
+      local pick = require("mini.pick")
+      pick.setup()
+
+      require("mini.completion").setup()
+
+      vim.keymap.set("n", "<leader>e", function()
+        pick.builtin.files({
+          tool = "git", -- Uses git ls-files (tracked files only)
+        })
+      end, { desc = "Pick git tracked files" })
+
+      vim.keymap.set("n", "<leader>f", function()
+        pick.builtin.files() -- Defaults to 'rg' which respects .gitignore and global ignore
+      end, { desc = "Pick all files in cwd" })
+
+      local map_multistep = require("mini.keymap").map_multistep
+      map_multistep("i", "<Tab>", { "pmenu_next" })
+      map_multistep("i", "<S-Tab>", { "pmenu_prev" })
+      map_multistep("i", "<C-l>", { "pmenu_accept", "minipairs_cr" })
+      map_multistep("i", "<BS>", { "minipairs_bs" })
+
+      vim.keymap.set("i", "<C-e>", function()
+        MiniPick.builtin.buffer_lines({ scope = "current" })
+      end)
+    end,
+  },
   {
     "mrjones2014/smart-splits.nvim",
     lazy = false,
