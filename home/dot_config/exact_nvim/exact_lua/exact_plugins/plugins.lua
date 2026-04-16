035aabc7a3eac55acf0f06480f280c435471580c
Evan Sosenko <razorx@evansosenko.com>
2026-04-15 20:13:47 -0700

    Update SmartSplits binds

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/plugins.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/plugins.lua
@@ -45,16 +45,21 @@ return {
       local SmartSplits = require("smart-splits")
       SmartSplits.setup(opts)
 
+      -- resize splits
       vim.keymap.set("n", "<A-h>", SmartSplits.resize_left)
       vim.keymap.set("n", "<A-j>", SmartSplits.resize_down)
       vim.keymap.set("n", "<A-k>", SmartSplits.resize_up)
       vim.keymap.set("n", "<A-l>", SmartSplits.resize_right)
+      -- swapping splits
+      vim.keymap.set("n", "<A-H>", SmartSplits.swap_buf_left)
+      vim.keymap.set("n", "<A-J>", SmartSplits.swap_buf_down)
+      vim.keymap.set("n", "<A-K>", SmartSplits.swap_buf_up)
+      vim.keymap.set("n", "<A-L>", SmartSplits.swap_buf_right)
       -- moving between splits
       vim.keymap.set("n", "<C-h>", SmartSplits.move_cursor_left)
       vim.keymap.set("n", "<C-j>", SmartSplits.move_cursor_down)
       vim.keymap.set("n", "<C-k>", SmartSplits.move_cursor_up)
       vim.keymap.set("n", "<C-l>", SmartSplits.move_cursor_right)
-      vim.keymap.set("n", "<C-\\>", SmartSplits.move_cursor_previous)
     end,
   },
   {
