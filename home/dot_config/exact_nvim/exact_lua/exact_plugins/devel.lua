003a1a73980a8035018a630e1c9e42869f11da71
Evan Sosenko <razorx@evansosenko.com>
2026-04-20 23:22:47 -0700

    Move lsp to devel.lua

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/devel.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/devel.lua
@@ -3,6 +3,37 @@ local M = {}
 ---@module "lazy.types"
 ---@type LazySpec
 M.spec = {
+{
+    "neovim/nvim-lspconfig",
+    init = function()
+      vim.lsp.enable("clangd")
+
+      vim.lsp.enable("clojure_lsp")
+      -- TODO cljfmt, kondo ?
+
+      vim.lsp.enable("gopls")
+      vim.lsp.enable("golangci_lint_ls")
+
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("biome")
+
+      vim.lsp.enable("lua_ls")
+
+      vim.lsp.enable("stylua")
+
+      vim.lsp.enable("phpactor")
+      -- TODO mago ?
+
+      vim.lsp.enable("pyright")
+      vim.lsp.enable("ruff")
+
+      vim.lsp.enable("ruby_lsp")
+      vim.lsp.enable("rubocop")
+
+      vim.lsp.enable("bashls")
+      vim.lsp.enable("fish_lsp")
+    end,
+  },
   {
     "folke/trouble.nvim",
     ---@module "trouble"
