9012fa2bf4b4645b46bdaa026a1a9d4882a31f66
Evan Sosenko <razorx@evansosenko.com>
2026-04-21 23:58:53 -0700

    Swap jump2d for flash

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/editor.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/editor.lua
@@ -222,14 +222,25 @@ M.spec = {
     "nvim-mini/mini.pairs",
     opts = {},
   },
-  {
-    "nvim-mini/mini.jump2d",
-    opts = {
-      mappings = {
-        start_jumping = "<CR>",
-      },
-    },
+{
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {
+    modes = {
+    char = {
+        enabled = false,
+      }
+    }
   },
+  keys = {
+    { "S", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "R", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+},
   {
     "nvim-mini/mini.cmdline",
     init = function()
