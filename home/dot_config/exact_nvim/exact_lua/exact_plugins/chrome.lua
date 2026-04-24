c62ed69ea023f906428d72e38673f4acae2cb30e
Evan Sosenko <razorx@evansosenko.com>
2026-04-23 21:02:59 -0700

    Add delay to which-key

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/chrome.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/chrome.lua
@@ -25,9 +25,7 @@ M.spec = {
     dependencies = { "nvim-mini/mini.icons" },
     event = "VeryLazy",
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+      delay = 500,
     },
     keys = {
       {
