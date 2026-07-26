83e9232251866be1a5354d9ecfe5ca860fb83f94
Evan Sosenko <razorx@evansosenko.com>
2026-07-26 16:35:21 -0500

    Enable multiple local extensions

--- home/dot_config/pi/extensions/exact_local/index.ts
+++ home/dot_config/pi/extensions/exact_local/index.ts
@@ -1,1 +1,8 @@
-export { default } from './cursor-down-or-newline.ts'
+import type { ExtensionAPI, ExtensionFactory } from '@earendil-works/pi-coding-agent'
+import { default as cursorDownOrNewLine } from './cursor-down-or-newline.ts'
+
+const extensions: ExtensionFactory[] = [cursorDownOrNewLine]
+
+export default async function exactLocal(pi: ExtensionAPI): Promise<void> {
+  for (const extension of extensions) await extension(pi)
+}
