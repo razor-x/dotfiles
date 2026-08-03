37f5f3a16011b7186c950efda8d095cd2ab97e28
Evan Sosenko <razorx@evansosenko.com>
2026-08-03 12:09:20 -0700

    Use tree-sitter-manager

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/devel.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/devel.lua
@@ -35,11 +35,12 @@ M.spec = {
     end,
   },
   {
-    "nvim-treesitter/nvim-treesitter",
+    "romus204/tree-sitter-manager.nvim",
     lazy = false,
-    build = ":TSUpdate",
-    config = function()
-      local parsers = {
+    ---@module "tree-sitter-manager"
+    ---@type table
+    opts = {
+      ensure_installed = {
         "bash",
         "c",
         "clojure",
@@ -80,45 +81,8 @@ M.spec = {
         "vimdoc",
         "xml",
         "yaml",
-      }
-
-      require("nvim-treesitter").install(parsers)
-
-      vim.api.nvim_create_autocmd("FileType", {
-        group = vim.api.nvim_create_augroup("treesitter", { clear = true }),
-        callback = function(event)
-          local filetype = vim.bo[event.buf].filetype
-          local language = vim.treesitter.language.get_lang(filetype) or filetype
-
-          if not vim.list_contains(parsers, language) then
-            return
-          end
-
-          local has_parser = pcall(vim.treesitter.get_parser, event.buf, language)
-          if not has_parser then
-            return
-          end
-
-          local function has_query(feature)
-            local ok, query = pcall(vim.treesitter.query.get, language, feature)
-            return ok and query ~= nil
-          end
-
-          if has_query("highlights") then
-            vim.treesitter.start(event.buf, language)
-          end
-
-          if has_query("folds") then
-            vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-            vim.wo[0][0].foldmethod = "expr"
-          end
-
-          if has_query("indents") then
-            vim.bo[event.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-          end
-        end,
-      })
-    end,
+      },
+    },
   },
   {
     "carderne/pi-nvim",
