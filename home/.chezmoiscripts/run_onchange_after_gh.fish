8f825293398f18e427c13a0e2bcfeb54f2fbaec5
Evan Sosenko <razorx@evansosenko.com>
2026-08-12 12:37:54 -0700

    Add gh remote add alias

--- home/.chezmoiscripts/run_onchange_after_gh.fish
+++ home/.chezmoiscripts/run_onchange_after_gh.fish
@@ -16,6 +16,30 @@ if type --query gh
     # Cut a new version via GitHub Actions.
     gh alias set --clobber ver \
         'workflow run version.yml --raw-field version="$1"'
+
+    # Manage git remotes and keep upstream (or origin) as gh default repo.
+    gh alias set --clobber remote '!f() {
+        if [ "$1" = add ] && [ "$#" -eq 3 ]; then
+            case "$3" in
+                *://*|git@*:*|/*) url="$3" ;;
+                */*) url=$(gh repo view "$3" --json sshUrl --jq .sshUrl) || return ;;
+                *) url="$3" ;;
+            esac
+            git remote add "$2" "$url" || return
+        else
+            git remote "$@" || return
+        fi
+
+        if git config --get remote.upstream.url >/dev/null; then
+            default=upstream
+        elif git config --get remote.origin.url >/dev/null; then
+            default=origin
+        else
+            return
+        fi
+
+        gh repo set-default "$default"
+    }; f "$@"'
 else
     echo 'Cannot setup GitHub CLI: gh not found.'
     return 1
