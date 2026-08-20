bb25ec20ed9af3ceb92401c53f129726d0399083
Evan Sosenko <razorx@evansosenko.com>
2026-08-19 21:39:19 -0700

    Tweak gh alias

--- home/.chezmoiscripts/run_onchange_after_gh.fish
+++ home/.chezmoiscripts/run_onchange_after_gh.fish
@@ -12,21 +12,25 @@ if type --query gh
 
     # Merge pull requests and delete the source branch.
     gh alias set --clobber m 'pr merge --merge --delete-branch'
+    gh alias set --clobber ma 'pr merge --merge --delete-branch --auto'
     gh alias set --clobber ms 'pr merge --squash --delete-branch'
-    gh alias set --clobber mrb 'pr merge --rebase --delete-branch'
+    gh alias set --clobber msa 'pr merge --squash --delete-branch --auto'
+    gh alias set --clobber mr 'pr merge --rebase --delete-branch'
+    gh alias set --clobber mra 'pr merge --rebase --delete-branch --auto'
 
     # Push the current branch and create a pull request.
-    gh alias set --clobber c '!git push --quiet && gh pr create --fill "$@"'
-    gh alias set --clobber cd '!git push --quiet && gh pr create --fill --draft "$@"'
+    gh alias set --clobber o '!git push --quiet && gh pr create --fill "$@"'
+    gh alias set --clobber od '!git push --quiet && gh pr create --fill --draft "$@"'
 
     gh alias set --clobber v 'pr view'
     gh alias set --clobber x 'pr view --web'
     gh alias set --clobber r 'pr ready'
+    gh alias set --clobber c 'pr checks'
     gh alias set --clobber w 'pr checks --watch'
     gh alias set --clobber diff 'pr diff'
     gh alias set --clobber close '!gh pr close "$(git branch --show-current)" "$@"'
 
-    gh alias set --clobber d 'dash'
+    gh alias set --clobber d dash
 
     # Clone repo.
     gh alias set --clobber clone 'repo clone'
