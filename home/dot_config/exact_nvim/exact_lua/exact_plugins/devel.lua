e96137087cc8a99481986217401c64fc21941a24
Evan Sosenko <razorx@evansosenko.com>
2026-08-21 00:09:29 -0700

    Add trouble / snacks integration

--- home/dot_config/exact_nvim/exact_lua/exact_plugins/devel.lua
+++ home/dot_config/exact_nvim/exact_lua/exact_plugins/devel.lua
@@ -287,6 +287,25 @@ M.spec = {
   },
   {
     "folke/trouble.nvim",
+    specs = {
+      {
+        "folke/snacks.nvim",
+        opts = function(_, opts)
+          return vim.tbl_deep_extend("force", opts or {}, {
+            picker = {
+              actions = require("trouble.sources.snacks").actions,
+              win = {
+                input = {
+                  keys = {
+                    ["<C-t>"] = { "trouble_open", mode = { "n", "i" } },
+                  },
+                },
+              },
+            },
+          })
+        end,
+      },
+    },
     ---@module "trouble"
     ---@type trouble.Config
     opts = {},
