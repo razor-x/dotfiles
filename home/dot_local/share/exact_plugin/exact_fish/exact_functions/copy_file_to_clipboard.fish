6af353068326a19c42c6d531a6aa319604dbb9ae
Evan Sosenko <razorx@evansosenko.com>
2026-08-26 00:53:35 -0700

    Make fish copy_file_to_clipboard work for subdirs

--- home/dot_local/share/exact_plugin/exact_fish/exact_functions/copy_file_to_clipboard.fish
+++ home/dot_local/share/exact_plugin/exact_fish/exact_functions/copy_file_to_clipboard.fish
@@ -1,14 +1,24 @@
 function copy_file_to_clipboard \
-    --description 'Fuzzy-search and copy file contents to clipboard'
+    --description 'Copy a file, or fuzzy-select one below the path at the cursor'
 
-    set --function files $argv
-    if test (count $files) -eq 0
-        set --function files \
-            (fzf --multi --preview 'bat --color=always --style=plain {}')
-        or return
+    set --function target (commandline --current-token | string unescape)
+    set target (string replace --regex '^~(?=/|$)' $HOME -- $target)
+    if test -z "$target"
+        set target .
     end
 
-    cat $files \
+    if path is -d $target
+        set --function files \
+            (fd --type file --hidden --exclude .git . $target | fzf --multi --preview 'bat --color=always --style=plain {}')
+        or return
+    else if path is -f $target
+        set --function files $target
+    else
+        return
+    end
+
+    cat -- $files \
         | string collect \
         | fish_clipboard_copy
+    and commandline --replace ''
 end
